8c087938af316e2de9bae109d65d640b0b99b672
IAFEnvoy <tanksheng545@126.com>
2024-06-02 08:52:21 +0800

    2.0.0

--- src/main/java/com/iafenvoy/thinkbeforedrop/ThinkBeforeDrop.java
+++ src/main/java/com/iafenvoy/thinkbeforedrop/ThinkBeforeDrop.java
@@ -5,14 +5,10 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Environment(EnvType.CLIENT)
 public class ThinkBeforeDrop implements ClientModInitializer {
     public static final String MOD_ID = "thinkbeforedrop";
-    public static final String MOD_NAME = "Think Before Drop";
-    public static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void onInitializeClient() {
